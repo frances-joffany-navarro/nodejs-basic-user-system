9b1630f79dcab5540cd4688720b7bbe796b9662c
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2022-01-08 22:04:31 +0100

    change to pool connection

--- services/connect.js
+++ services/connect.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE
+})
 
-const connectDB = (dbParams) => {
+/* const connectDB = (dbParams) => {
   //create a connection
   const connection = mysql.createConnection(dbParams)
   //open the MySQL connection
   connection.connect()
-}
+} */
 
-module.exports = connectDB
+module.exports = pool
